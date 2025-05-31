46aefdd42393ebcb285139f616d8f7ff28b5e149
saxd0ct0r <timothy.j.owen@gmail.com>
2025-05-31 00:21:12 -0700

    Tinkering with branches

--- ZyBooks Examples/4_19_LAB.py
+++ ZyBooks Examples/4_19_LAB.py
@@ -4,10 +4,14 @@
 
 input_year = int(input())
 
-is_leap_year = True if (
-    input_year % 4 == 0 and not input_year % 100 == 0
-) or (input_year % 400 == 0) else False
+# is_leap_year = True if (
+#     input_year % 4 == 0 and not input_year % 100 == 0
+# ) or (input_year % 400 == 0) else False
 
-result = "leap year" if is_leap_year else "not a leap year"
+result = "leap year" if (
+    not input_year % 4 and input_year % 100
+) or (
+    not input_year % 400
+) else "not a leap year"
 
 print(input_year, "-", result)
