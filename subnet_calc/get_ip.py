61b84814f3a7ec02c2cc673d34265d63dfa9f5b5
saxd0ct0r <timothy.j.owen@gmail.com>
2025-06-03 00:37:11 -0700

    added return value to get_ip

--- subnet_calc/get_ip.py
+++ subnet_calc/get_ip.py
@@ -4,20 +4,34 @@
 # returns a list with four octets and CIDR code as ints, Ex. [192, 168, 30, 10, 24]
 
 def get_ip_address():
-    while True:
-        user_response = input("Enter IP/CIDR (X to exit): ")
-        if user_response.lower() == "x":
-            return -1
-        
-        ip_address = user_response.split(".")
-        if len(ip_address) == 4:
-            temp = ip_address[len(ip_address) - 1].split("/")
-            ip_address[len(ip_address) - 1] = temp[0]
-            cidr = temp[1]
-            print(ip_address, cidr)
-        else:
-            print("Error")
-            return -1
-        
 
-get_ip_address()
+    user_response = input("Enter IP/CIDR (X to exit): ")
+    if user_response.lower() == "x":
+        return -1
+    
+    ip_address = user_response.split(".")
+    if len(ip_address) == 4:
+        temp = ip_address[3].split("/")
+        ip_address[3] = temp[0]
+        cidr = temp[1]
+        print(ip_address, cidr)
+    else:
+        print("Error")
+        return -1
+    
+    index = 0
+    for octet in ip_address:
+        try:
+            octet = int(ip_address[index])
+            if 0 <= octet <= 255:
+                ip_address[index] = octet
+                index += 1
+            else:
+                return -1
+        except:
+            print("Invalid entry. IP address should be in form of ###.###.###.###/CIDR")
+            return -1
+        
+    return ip_address, cidr
+        
+print(get_ip_address())
