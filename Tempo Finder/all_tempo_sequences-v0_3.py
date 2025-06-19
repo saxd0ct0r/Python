99c5cd510a47c32d649ee1165c9633e066b537c5
saxd0ct0r <timothy.j.owen@gmail.com>
2025-06-18 17:02:34 -0700

    Refactoring tempo sequence calculator

--- Tempo Finder/all_tempo_sequences-v0_3.py
+++ Tempo Finder/all_tempo_sequences-v0_3.py
@@ -3,6 +3,8 @@
 # version 0.2
 
 low, high = "low", "high"
+# define preset tempos and step sizes in a tuple so they don't get changed
+# by accident
 TEMPOS = (
           30, 31.5, 33, 34.5, 36, 38, 40, 42,
           44, 46, 48, 50, 52, 54, 56, 58,
@@ -15,8 +17,8 @@ TEMPOS = (
 STEP_SIZES = (16, 8, 4, 2, 1, 1)
 quit = False
 
-while True:
-    while True:
+while True:     # Main loop, keep running with user-selected starting tempo
+    while True: # repeat until valid input
         try:
             start_tempo = int(input("Enter a start tempo (30-240, 0 to exit): "))
             if start_tempo == 0:
@@ -33,11 +35,11 @@ while True:
     if quit:
         break
     
-    if start_tempo not in TEMPOS:
-        for tempo in TEMPOS[::-1]:
+    if start_tempo not in TEMPOS:   # slot start tempo into preset options
+        for tempo in TEMPOS[::-1]:  # Find the first preset that is lower
             if tempo < start_tempo:
                 start_tempo = tempo
-                print(f"Closest match is {start_tempo}")
+                print(f"Closest match is {start_tempo}\n")
                 break
     
     start_tempo_index = TEMPOS.index(start_tempo)
@@ -47,25 +49,30 @@ while True:
     for i in range(2 ** (len(STEP_SIZES))): # All the variants
         tempo_index = start_tempo_index
         step_size_index = 0
-        step_size = STEP_SIZES[step_size_index]
-        tempo = TEMPOS[tempo_index]
+
         maxxed =  False
-        all_successes = True
-        last_tempo = 0
+        perfect_run = True
         
         print(f"{i}:", end=" ")
-        for step_size_index in range(len(STEP_SIZES)):
-            step_size = STEP_SIZES[step_size_index]
+
+        while step_size_index < len(STEP_SIZES):
             tempo = TEMPOS[tempo_index]
+            step_size = STEP_SIZES[step_size_index]
 
-            too_low = tempo_index - step_size < 0
-            too_high = tempo_index + step_size > len(TEMPOS) - 1
-
-            tempo_increases = True if i & (2 ** ((len(STEP_SIZES) - 1) - step_size_index)) else False
-            if tempo != last_tempo:
-                print(f"{tempo}", end=" ")
+            # too_low = tempo_index - step_size < 0
+            # Using binary mask to represent going up or down in tempo to
+            # converge on target tempo.
+            mask_for_step_sizes = 2 ** ((len(STEP_SIZES) - 1) - step_size_index)
+            tempo_increases = True if i & mask_for_step_sizes else False
+            print(f"{tempo}", end=" ")
 
             if tempo_increases:
+                too_high = tempo_index + step_size > len(TEMPOS) - 1
+                while too_high or step_size_index < len(STEP_SIZES) - 1:
+                    step_size_index += 1
+                    step_size = STEP_SIZES[step_size_index]
+                    too_high = tempo_index + step_size > len(TEMPOS) - 1
+
                 if tempo == upper_bound:
                     print("You've maxxed your tempo. Good job!")
                     maxxed = True
@@ -76,7 +83,7 @@ while True:
                     last_tempo = tempo
         
             else:
-                all_successes = False
+                perfect_run = False
                 if tempo == lower_bound:
                     print("Try breaking things up.")
                     break
@@ -94,7 +101,7 @@ while True:
             tempo = TEMPOS[tempo_index - 1]
 
         print(tempo)
-        if all_successes and not maxxed:
+        if perfect_run and not maxxed:
             tempo = TEMPOS[tempo_index]
             print(f"All successes. Try at {tempo}")
         # tempo_index = TEMPOS.index(start_tempo)
