0cb9208d0c27c89a97fe070fca0f4f106b8a816a
saxd0ct0r <timothy.j.owen@gmail.com>
2025-06-23 17:12:46 -0700

    adding functionality to tempo finder: review

--- Tempo Finder/tempo_finder_review.py
+++ Tempo Finder/tempo_finder_review.py
@@ -2,7 +2,6 @@
 # 23 June 2025
 # version 0.2
 
-# low, high = "low", "high"
 # define preset tempos and step sizes in a tuple so they don't get changed
 # by accident
 TEMPOS = (
@@ -19,21 +18,11 @@ lower_bound = TEMPOS[0]
 upper_bound = TEMPOS[-1]
 quit = False
 
-def find_tempo(start_tempo = 60, first_time = True, start_perfect = True):
+def find_tempo(start_tempo = 60, initial_step_size_index = 0, start_perfect = True):
 
-    # print(f"Starting tempo: ~{start_tempo}\n")
-
-    if start_tempo in TEMPOS:       # slot start tempo into preset options
-        pass
-    else:
-        for tempo in TEMPOS[::-1]:  # Find the first preset that is lower
-            if tempo < start_tempo:
-                start_tempo = tempo
-                break
-    
-    tempo_index = TEMPOS.index(start_tempo)
-    step_size_index = 0
-
+    # resolve tempo to available slots
+    tempo_index = resolve_tempo(start_tempo)
+    step_size_index = initial_step_size_index
     maxxed =  False
     perfect_run = start_perfect      # Used to keep track of a string of all successes
     
@@ -43,6 +32,7 @@ def find_tempo(start_tempo = 60, first_time = True, start_perfect = True):
         tempo_increases = check_success(tempo)
 
         if tempo_increases:
+            # Check upper bound
             if tempo == upper_bound:    # You could go faster, but really?
                 maxxed = True
                 break
@@ -65,18 +55,12 @@ def find_tempo(start_tempo = 60, first_time = True, start_perfect = True):
             tempo_index -= step_size
                 
         step_size_index += 1
-    
-    # tempo = TEMPOS[tempo_index]
-    if tempo_increases and not maxxed:
-        # tempo = TEMPOS[tempo_index - 1]
-        tempo_index -= 1
 
-    # # If it was a perfect run, you get one more shot that will end up potentially
-    # # quadrupling the tempo.
-    # if perfect_run and not maxxed:
-    #     tempo = TEMPOS[tempo_index]
-    #     print(f"All successes. Try at {tempo}")
-    
+    # The last repetition: success means you have reached the target, not that 
+    # you should increment the tempo
+    if tempo_increases and not maxxed:
+        tempo_index -= 1   
+
     return tempo_index, perfect_run
 
 # Checks to see if the next step_size takes the tempo out of bounds. If so, cut 
@@ -105,6 +89,7 @@ def check_lower_bound(tempo_index, step_size_index):
 
     return step_size_index, step_size
 
+# Gets user input about success or failure at given tempo
 def check_success(tempo):
     success = None
     user_input = input(f"Attempt at {tempo}bpm. Success? (y/n is default): ")
@@ -117,17 +102,26 @@ def check_success(tempo):
     
     return success
 
+# This function resolves the input to the index that most closely matches
+def resolve_tempo(tempo):
+    if tempo in TEMPOS:       # slot start tempo into preset options
+        return TEMPOS.index(tempo)
+    else:
+        for real_tempo in TEMPOS[::-1]:  # Find the first preset that is lower
+            if real_tempo < tempo:
+                return TEMPOS.index(real_tempo)
+  
 while True:     # Main loop, keep running until user chooses to exit
     while True: # repeat until valid input
-        try:
-            first_time = False
-            start_tempo = input("Enter a start tempo (30-240,"
-                                " [Enter] for first time,"
-                                " 0 to exit): ")
-            if start_tempo == "":
-                first_time = True
-                break
+        first_time = False
+        start_tempo = input("Enter a start tempo (30-240,"
+                            " [Enter] for first time,"
+                            " 0 to exit): ")
+        if start_tempo == "":
+            first_time = True
+            break
 
+        try:
             start_tempo = int(start_tempo)
             if start_tempo == 0:
                 quit = True
@@ -145,9 +139,23 @@ while True:     # Main loop, keep running until user chooses to exit
 
     if first_time:
         tempo_index, perfect_run = find_tempo()
+        if perfect_run:
+            tempo_index += 1 if check_success(TEMPOS[tempo_index + 1]) else 0
         print(TEMPOS[tempo_index])
     else:
-        # TODO play at start_tempo, note success or failure
-        print("Review at starting tempo first")
-        print(find_tempo(start_tempo / 2))
-        # find tempo starting from 1/2 of start_tempo
+        first_trial_success = check_success(start_tempo)
+        tempo_index = resolve_tempo(start_tempo)
+        step_size_index = 0
+        step_size_index, step_size = check_lower_bound(tempo_index, step_size_index)
+        tempo_index -= step_size
+        step_size_index += 1
+        tempo = TEMPOS[tempo_index]
+        
+        tempo_index, perfect_run = find_tempo(tempo, step_size_index, first_trial_success)
+        if perfect_run:
+            if check_success(TEMPOS[tempo_index + 1]):
+                tempo_index += 1
+                
+                
+
+        print(TEMPOS[tempo_index])
